70ee985ab43129d35f0f5c0bc92627cad9e0ddc2
gitmann-ayt <aytesmasuhail@email.com>
2026-04-28 14:50:33 +0500

    feat: add S3 document upload bucket via CDK

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3 bucket where users will upload their documents
+    const documentBucket = new s3.Bucket(this, 'DocumentsBucket', {
+      bucketName: `smartdocs-uploads-${this.account}`,
+      versioned: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      cors: [
+        {
+          allowedMethods: [s3.HttpMethods.PUT, s3.HttpMethods.POST],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*'],
+          maxAge: 3000,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Output the bucket name so we can reference it later
+    new cdk.CfnOutput(this, 'DocumentsBucketName', {
+      value: documentBucket.bucketName,
+      description: 'S3 bucket for document uploads',
+      exportName: 'SmartDocsUploadsBucket',
+    });
   }
 }
